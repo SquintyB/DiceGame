bc24c56b58f890821b3be8bcc2c0f56303442c08
SquintyBiggins <SquintyB.development@gmail.com>
2020-10-18 22:33:22 -0400

    Fully Working Game(I think)

--- app.js
+++ app.js
@@ -12,40 +12,67 @@ GAME RULES:
 // Variables
 // Declarations
 let scores, roundScore, activePlayer, gameStart;
-// Initialize Variables
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+let diceDisplay = document.querySelector(".dice");
+resetGame();
 
-
-// Get Document Objects
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = "0";
-document.getElementById('score-1').textContent = "0";
-document.getElementById('current-0').textContent = "0";
-document.getElementById('current-1').textContent = "0";
-
-document.querySelector('.btn-roll').addEventListener("click", function(){
-// Generate Random number
+// Roll dice button event listener
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  // Generate Random number
   var diceRoll = Math.floor(Math.random() * 6) + 1;
-// Unhide dice element and display proper graphic
-  let diceDisplay = document.querySelector(".dice");
+  // Unhide dice element and display proper graphic
   diceDisplay.style.display = "block";
   diceDisplay.src = `dice-${diceRoll}.png`;
-//update round score and add to global score or end turn
-if(diceRoll !== 1){
+  //update round score and add to global score or end turn
+  if (diceRoll !== 1) {
     // add score
     roundScore += diceRoll;
     document.getElementById(`current-${activePlayer}`).textContent = roundScore;
-}else{
+  } else {
+    nextPlayer();
+  }
+});
+
+// Hold button Event listener
+document.querySelector(".btn-hold").addEventListener("click", () => {
+  //Add round score to global score
+  scores[activePlayer] += roundScore;
+  // Update UI
+  document.getElementById(`score-${activePlayer}`).textContent =  scores[activePlayer];
+  // Check for win condition/ switch player
+  if (scores[activePlayer] >= 100) {
+    alert(`Player ${activePlayer + 1} Wins`);
+    resetGame();
+  } else {
+    nextPlayer();
+  }
+});
+
+// New Game event listener
+document.querySelector(".btn-new").addEventListener("click", () => {
+    resetGame();
+})
+
+function nextPlayer(){
     roundScore = 0;
     document.getElementById(`current-${activePlayer}`).textContent = "0";
-    // If 
-    activePlayer === 0 ? activePlayer = 1: activePlayer = 0; //Ternary Operator example
+    // If
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0); //Ternary Operator example
     // Toggle active indicator
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-    diceDisplay.style.display = 'none';
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDisplay.style.display = "none";
 }
 
-});
+
+function resetGame(){
+// Initialize Variables
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+// Get Document Objects
+document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+}
